9effb33c3b685d0c556ede4b4f66570f56f248e1
Mohammad Sulaiman <smensulaiman007@gmail.com>
2024-03-15 11:12:06 +0900

    final refactor

---
+++ src/configs/path_constants.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Author: SULAIMAN
+ * Email: sulaiman@sendajapan.com
+ * Website: https://www.sulaimansan.com
+ *
+ * Description: Brief description of the code or file.
+ *
+ * Date: 3/15/2024
+ */
---
+++ src/configs/routes.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Author: SULAIMAN
+ * Email: sulaiman@sendajapan.com
+ * Website: https://www.sulaimansan.com
+ *
+ * Description: Brief description of the code or file.
+ *
+ * Date: 3/15/2024
+ */
+
+declare(strict_types=1);
+ 
